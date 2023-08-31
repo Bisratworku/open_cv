0afc74a91a3b6c2ca389f06a8ca1922e032b6241
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-08-31 06:33:53 -0700

    color

--- chapter 1/ch_1.py
+++ chapter 1/ch_1.py
@@ -1,15 +1,23 @@
 import cv2
 #loading image
-image = cv2.imread("C:\\Users\\Bisrat worku\\Desktop\\OIP.jpg")
-cv2.imshow('image', image)
-cv2.waitKey(1000)
+img = "C:\\Users\\Bisrat worku\\Desktop\\OIP.jpg"
+image = cv2.imread(img)
+#cv2.imshow('image', image)
+#cv2.waitKey(1000)
 
 #loading grayscale image
 
-gray_image = cv2.imread("C:\\Users\\Bisrat worku\\Desktop\\OIP.jpg", cv2.IMREAD_GRAYSCALE)
-cv2.imshow("gray", gray_image)
-cv2.waitKey()
+gray_image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+#cv2.imshow("gray", gray_image)
+#cv2.waitKey()
 
 #saving images
 
 cv2.imwrite('gray.jpg', gray_image)
+
+#color chanales
+colors = [x for x in dir(cv2) if x.startswith('COLOR_')]
+print(colors[:5])
+col_1 = cv2.imread(img, cv2.COLOR_BAYER_BG2RGB)
+cv2.imshow("1", col_1)
+cv2.waitKey() 
